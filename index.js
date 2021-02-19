14eb4d2f0fc20d576987a975b6ed994cf45250ed
mb-ui <mohsen.biglari.js@gmail.com>
2021-02-19 21:48:17 +0330

    update index.js and package.json

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
+const open = require('open')
+    , express = require('express')
     , path = require('path')
     , app = express()
-    , port = 8083;
+    , port = 5000;
 app.use(express.static(path.join(__dirname, 'public')))
     .get('/', (req, res) => {
         res.sendfile('./public/index.html');
     }).listen(port, () => {
         console.log(`Example app listening at http://localhost:${port}`);
+        (async () => {
+            await open(`http://localhost:${port}`);
+        })();
     });
 module.exports = app;
